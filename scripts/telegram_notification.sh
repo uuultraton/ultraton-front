3695cbac48ca94a5b15dc04102f31485947e543d
ohexus <moskalenko.taras.d@gmail.com>
2020-08-22 22:06:49 +0300

    now notifications are sent after build

--- scripts/telegram_notification.sh
+++ scripts/telegram_notification.sh
@@ -2,6 +2,12 @@ BOT_URL="https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage"
 
 PARSE_MODE="HTML"
 
+if [ $TRAVIS_TEST_RESULT -ne 0 ]; then
+  build_status="failed"
+else
+  build_status="succeeded"
+fi
+
 send_msg() {
   curl -s -X POST ${BOT_URL} -d chat_id=$TELEGRAM_CHAT_ID \
   -d text="$1" -d parse_mode=${PARSE_MODE}
@@ -10,6 +16,7 @@ send_msg() {
 send_msg "
 -------------------------------------
 UltraTon-FE
+Build ${build_status}!
 Repository:  https://github.com/${TRAVIS_REPO_SLUG}
 Heroku:  ${HEROKU_URL}
 
