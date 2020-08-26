8b20ed8b662885d397b182619a1ff2b1190e8afb
max <maxonchil@gmail.com>
2020-08-26 17:50:20 +0300

    deleted non usable element

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -147,9 +147,6 @@ export default function Header() {
                 <MenuItem onClick={handleHome}>Home</MenuItem>
                 <MenuItem onClick={handleProfile}>Profile</MenuItem>
                 <MenuItem onClick={handlePlay}>Play</MenuItem>
-                <MenuItem onClick={() => history.push('/modal')}>
-                  Modal
-                </MenuItem>
                 <MenuItem onClick={handleLogout}>Logout</MenuItem>
               </Menu>
             </div>
